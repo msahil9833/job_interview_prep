7eb36818616961a7df30294f891194a50b119a45
msahil9833 <imsahil9833@gmail.com>
2025-04-07 22:28:13 +0530

    setup app, file and folder structure,routing and auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AuthFrom from "@/components/AuthFrom";
+const Page =() => {
+    return <AuthFrom type="sign-in"/>
+}
+
+export default Page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AuthFrom from "@/components/AuthFrom";
+const Page =() => {
+    return  <AuthFrom type="sign-up"/>
+}
+
+export default Page
